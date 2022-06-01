f341c5053f8d4666302e97ff5ac0a19236e1d356
Dima <mail@parkee.ru>
2022-06-02 00:52:39 +0300

    Simulation for tooltip position calculation is finished

--- trains.py
+++ trains.py
@@ -1,11 +1,13 @@
 import numpy as np
 import math
 
-#DRAFT TESTS TO IMPLEMENT IDEAS
+# DRAFT TESTS TO IMPLEMENT IDEAS
 # READ https://studref.com/472293/tehnika/matrichnye_metody_preobrazovaniya_koordinat_robototehnike?
 
+# NB! It is importantant to take into account CW/CCW rotation for axis in future
+
 pnt = np.array([[0], [0], [0], [1]])
-alpha = math.radians(0)
+alpha = math.radians(-2)
 print(alpha)
 
 
@@ -19,7 +21,7 @@ Z
 '''
 
 #Tooltip shift matrix
-motor1_x_shift = 53.0 + 64.5
+motor1_x_shift = 117.5 #53.0 + 64.5  
 motor1_y_shift = -39.5
 motor1_z_shift = 0
 
@@ -37,8 +39,8 @@ Tm1l = np.array([   [1, 0,  0,  0],
                     [0, 0,  0,  1]  ])
 
 #Angular rotation 
-Tm1a = np.array([   [1, 0               , 0                 , 0],
-                    [0, math.cos(alpha) , -math.sin(alpha)  , 0],
+Tm1a = np.array([   [1, 0               , 0                 , 0],       #X axis (no changes because of angle)
+                    [0, math.cos(alpha) , -math.sin(alpha)  , 0],       #Rotation in YZ plane
                     [0, math.sin(alpha) ,  math.cos(alpha)  , 0],
                     [0, 0               , 0                 , 1]    ])
 
@@ -47,7 +49,7 @@ T = np.matmul(Tm1a, Tm1l)   #Linear shift mounted on motor
 T = np.matmul(T, Tm1s)      #Linear shifts because of design
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-fi = math.radians(0)
+fi = math.radians(10)
 print(fi)
 
 motor2_x_shift = 0
@@ -68,9 +70,9 @@ Tm2l = np.array([   [1, 0,  0,  0],
                     [0, 0,  0,  1]  ])
 
 #Angular rotation 
-Tm2a = np.array([   [math.cos(fi) , 0 , math.sin(fi) , 0],
+Tm2a = np.array([   [math.cos(fi) , 0 , math.sin(fi) , 0],          #Rotation affects X axis
                     [0            , 1 , 0            , 0],
-                    [-math.sin(fi), 0 , math.cos(fi) , 0],
+                    [-math.sin(fi), 0 , math.cos(fi) , 0],          #Rotation affects Z axis (ie XZ plane)
                     [0            , 0 , 0            , 1]    ])
 
 T = np.matmul(T, Tm2a)   #Linear shift mounted on motor
@@ -78,7 +80,7 @@ T = np.matmul(T, Tm2l)   #Linear shift mounted on motor
 T = np.matmul(T, Tm2s)   #Linear shifts because of design
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-theta = math.radians(-0)
+theta = math.radians(-90)
 print(theta)
 
 motor3_x_shift =  0
@@ -98,15 +100,14 @@ Tm3l = np.array([   [1, 0,  0,  0],
                     [0, 0,  1,  0],
                     [0, 0,  0,  1]  ])
 
-#Angular rotation 
-Tm3a = np.array([   [math.cos(theta) , -math.sin(theta) , 0, 0],                    
-                    [math.sin(theta) , math.cos(theta)  , 0, 0],
-                    [0               , 0                , 1, 0],                    
-                    [0               , 0                , 0, 1]    ])
+#Angular rotation
+Tm3a = np.array([   [math.cos(theta) , 0 , math.sin(theta) , 0],            #rotation affects X axis
+                    [0               , 1 , 0               , 0],
+                    [-math.sin(theta), 0 , math.cos(theta) , 0],            #affects Z axis
+                    [0               , 0 , 0               , 1]    ])
 
-
-#T = np.matmul(T, Tm3a)   #Linear shift mounted on motor
-T = np.matmul(Tm3a, Tm3l)   #Linear shift mounted on motor
+T = np.matmul(T, Tm3a)   #Linear shift mounted on motor
+T = np.matmul(T, Tm3l)   #Linear shift mounted on motor
 T = np.matmul(T, Tm3s)   #Linear shifts because of design
 
 
