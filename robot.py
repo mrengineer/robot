78217a045ea2039f4f4845d0582d2941f439c8f6
Dima <mail@parkee.ru>
2022-06-03 14:49:42 +0300

    Robot class has been added for development

--- robot.py
+++ robot.py
@@ -1,4 +1,5 @@
 import copy, time, serial, math
+from xmlrpc.client import Boolean
 import pytest
 from struct import *
 
@@ -18,13 +19,30 @@ CMD_MOTOR_MODEL                = 0x12        #Read driver and motor model comman
 class Motor():
     serial_port = None
     id: hex     = 0x00
+    name: str   = None
 
-    def __init__(self, id: hex, serial_port):
+    tolerance: float = 0.1 #deg. Used for different operatins like wait_stop Must be > 0.01!
+
+    # Simulation rotation
+    sim_CW: bool       = True
+
+    # Tooltip shift. Motor's zero point is bottom axis of motor.
+    sim_x_shift: float = 0.0
+    sim_y_shift: float = 0.0
+    sim_z_shift: float = 0.0
+
+
+    def __init__(self, id: hex, serial_port, tolerance: float, name: str):
         assert id >= 0
+        assert tolerance >= 0.01
         assert serial_port != None
+        assert name != None
 
         self.serial_port = serial_port
         self.id          = id
+        self.tolerance   = tolerance
+        self.name        = name
+
     
     def __read_responce(self, bytes_expect: int):
         self.serial_port.timeout = 0.1
@@ -35,7 +53,6 @@ class Motor():
             raise TimeoutError(f"Motor does not responded {bytes_expect} byte(s), result is '{res}'")
 
         return res
-
     
     # Writes the current encoder position of the motor into ROM as the initial position.  Attention:
     # 1. This command needs to restart to take effect.
@@ -45,12 +62,13 @@ class Motor():
         header_crc  = (CMD_HEADER + CMD_SET_ZERO + self.id + data_length) % 256
         
         snd = bytearray(pack('<BBBBB', CMD_HEADER, CMD_SET_ZERO, self.id, data_length, header_crc))
-        self.serial_port.write(snd)
-        #time.sleep(.01)  #give the serial port sometime to receive the data
+        self.serial_port.write(snd)        
         res = self.__read_responce(26)      #wait 26 bytes
 
     def abs_multi_loop_angle_speed(self, angle: float, speed: float):
-        assert speed > 0
+        assert speed > 0, "Speed must be grater than zero"
+
+        #if not self.CW:  angle = -1 * angle
         
         data_length = 0x0C
         
@@ -66,13 +84,16 @@ class Motor():
         
         self.serial_port.write(snd)
 
-        #time.sleep(.01)  #give the serial port sometime to receive the data
         res = self.__read_responce(13)      #wait 13 bytes
 
         return res
 
     def inc_angle_speed(self, angle: float, speed: float):
+        assert speed > 0, "Speed must be grater than zero"
+
         data_length = 0x08
+
+        #if not self.CW:  angle = -1 * angle
         
         header_crc  = (CMD_HEADER + CMD_INC_ANGLE_SPEED + self.id + data_length) % 256
 
@@ -87,10 +108,7 @@ class Motor():
         print(" ".join(map(lambda b: format(b, "02x"), snd)))
         self.serial_port.write(snd)
 
-        #time.sleep(.01)  #give the serial port sometime to receive the data
-
         res = self.__read_responce(13)      #wait 13 bytes
-
 
         return res
 
@@ -112,8 +130,11 @@ class Motor():
         b.append(res[6])
         
         angle = unpack("H", b)
-        
-        return float(angle[0]/100)
+
+        r_angle = float(angle[0]/100)        
+        #if not self.CW:  r_angle = -1 * r_angle
+
+        return r_angle
 
     # 0 ... INF deg
     def get_multi_loop_angle(self):
@@ -130,23 +151,25 @@ class Motor():
         
         angle = unpack("HHHH", res[5:13])
         
-        return angle[0]/100
+        r_angle = float(angle[0]/100)        
+        #if not self.CW:  r_angle = -1 * r_angle
 
-    #blocks code execution till motor stop (angle do not change)
+        return r_angle
+
+    #blocks code execution till motor stop (angle do not change because of ANY reason)
     #tolerance to detect angle similarity,  request_period to reduce ammout of requests
-    def wait_stop(self, tolerance: float, request_period: float, timeout: float):
+    def wait_stop(self, request_period: float, timeout: float):
         prev_value  = 9999999
         start       = time.time()
 
         assert request_period   >= 0.01
-        assert tolerance        >= 0.01
-        assert timeout          >= 0.1
+        assert timeout          >= 0.1, "Timeout must be > 0.1 sec"
 
         while (time.time() - start < timeout):
             cur_angle   = self.get_multi_loop_angle()
             delta       = abs(prev_value-cur_angle)
             
-            if (delta < tolerance): return
+            if (delta < self.tolerance): return
 
             prev_value = cur_angle
             time.sleep(request_period)
@@ -155,11 +178,40 @@ class Motor():
 
 
 class Robot():
-    __axes = list()
+    __motors = list()
 
     def __init__(self) -> None:
         pass
 
+    def add_motor(self, id: hex, serial_port, tolerance: float, name: str):
+        new_motor = Motor(id, serial_port, tolerance, name)
+        self.__motors.append(new_motor)
+        return new_motor
+
+    def goto_zero(self, speed: float):
+        for motor in self.__motors:
+            motor.abs_multi_loop_angle_speed(0, speed)
+        self.wait_stop()
+
+    def goto_abs_multi_loop_angles_speeds(self, angles: list, speeds: list):
+        assert len(angles) == len(speeds) == len(self.__motors), "Ammout of motors, speeds and angles must be same"
+
+        for motor, angle, speed in zip(self.__motors, angles, speeds):
+            try:
+                angle = float(angle)
+                speed = float(speed)
+            except ValueError as e:
+                print("Error in robot.goto_abs_multi_loop_angles_speeds", str(e))
+                exit()
+            motor.abs_multi_loop_angle_speed(angle, speed)
+        self.wait_stop()
+
+    #blocks code execution till ALL motors stop (angle do not change because of ANY reason)
+    #tolerance to detect angle similarity,  request_period to reduce ammout of requests
+    def wait_stop(self, request_period: float = 0.1, motor_timeout: float = 15):
+        for motor in self.__motors:
+            motor.wait_stop(request_period, motor_timeout)
+            
 # MAIN
 def main():
     try:
@@ -172,34 +224,24 @@ def main():
             #timeout=1
         )
     except Exception as e:
-        print ("error open serial port: " + str(e))
+        print ("Error open serial port: " + str(e))
         exit()
 
     if ser.isOpen():
         try:
             ser.flushInput()    #flush input buffer, discarding all its contents
             ser.flushOutput()   #flush output buffer, aborting current output
-
             
-            motor1 = Motor(0x01, ser)
-            motor2 = Motor(0x02, ser)
-            motor3 = Motor(0x03, ser)
-
+            robot = Robot()
+            robot.add_motor(0x01, ser, 0.1, "X")
+            robot.add_motor(0x02, ser, 0.1, "Y")
+            robot.add_motor(0x03, ser, 0.1, "Y2CCW")
 
 
             #GO ZERO pos
-            #motor1.abs_multi_loop_angle_speed(0.0, 10)
-            #motor2.abs_multi_loop_angle_speed(0.0, 10)
-            #motor3.abs_multi_loop_angle_speed(0.0, 30)
-
-            motor1.abs_multi_loop_angle_speed(0, 10)
-            motor2.abs_multi_loop_angle_speed(0, 10)
-            motor3.abs_multi_loop_angle_speed(0, 30)
-
-            motor1.wait_stop(0.1, 0.1, 15.0)
-            motor2.wait_stop(0.1, 0.1, 15.0)
-            motor3.wait_stop(0.1, 0.1, 15.0)
-
+            robot.goto_zero(10)
+                        
+            
             #save zero positions for axes
             #motor1.set_zero_cur_position()
             #motor2.set_zero_cur_position()
@@ -208,17 +250,19 @@ def main():
             #Moves in cycle
             while True:
 
-                motor1.abs_multi_loop_angle_speed(0.0, 10)
-                motor2.abs_multi_loop_angle_speed(0.0, 10)
-                motor3.abs_multi_loop_angle_speed(-90.0, 10)
+                #motor1.abs_multi_loop_angle_speed(0.0, 10)
+                #motor2.abs_multi_loop_angle_speed(0.0, 10)
+                #motor3.abs_multi_loop_angle_speed(-90.0, 50)
 
-                motor1.wait_stop(0.1, 0.1, 15.0)
-                motor2.wait_stop(0.1, 0.1, 15.0)
-                motor3.wait_stop(0.1, 0.1, 15.0)
+                robot.goto_abs_multi_loop_angles_speeds([15, 30, 30], [40, 60, 60])
 
                 time.sleep(1)
+                robot.goto_abs_multi_loop_angles_speeds([0, 0, 0], [40, 60, 60])
 
-                print("M1 ", motor1.get_multi_loop_angle(), ", M2", motor2.get_multi_loop_angle(), ", M3 ", motor3.get_multi_loop_angle())
+                time.sleep(1)
+                robot.goto_abs_multi_loop_angles_speeds([-10, -30, -30], [40, 60, 60])
+
+                #print("M1 ", motor1.get_multi_loop_angle(), ", M2", motor2.get_multi_loop_angle(), ", M3 ", motor3.get_multi_loop_angle())
                 time.sleep(1)
 
             ser.close()
--- trains.py
+++ trains.py
@@ -1,117 +1,146 @@
 import numpy as np
-import math
+import math, os
 
 # DRAFT TESTS TO IMPLEMENT IDEAS
 # READ https://studref.com/472293/tehnika/matrichnye_metody_preobrazovaniya_koordinat_robototehnike?
 
-# NB! It is importantant to take into account CW/CCW rotation for axis in future
-
-pnt = np.array([[0], [0], [0], [1]])
-alpha = math.radians(-2)
-print(alpha)
-
 
 '''
- X
- ^
- |
- /-----> Y
-/
-Z 
+      X
+      ^
+      |
+      /-----> Y
+     /
+    Z 
 '''
 
-#Tooltip shift matrix
-motor1_x_shift = 117.5 #53.0 + 64.5  
-motor1_y_shift = -39.5
-motor1_z_shift = 0
+# Motor settings are directly in code according prototype
+# It will be fixed while implementing Robot class
+def calc(alpha: float, fi: float, theta: float):
+    pnt = np.array([[0], [0], [0], [1]])    
 
-#Mechanical shift because of design
-Tm1s = np.array([   [1, 0,  0,  motor1_x_shift],
-                    [0, 1,  0,  motor1_y_shift],
-                    [0, 0,  1,  motor1_z_shift],
-                    [0, 0,  0,  1]  ])
+    print(f"""INPUT:
+        alpha   CW  X:             {alpha} deg
+        fi      CW  Y:             {fi} deg
+        theta   CCW Y after shift: {theta} deg
+{'-'*45}""")
 
+    motor1_CW = True
 
-#Linear move mounted on rotary AXIS
-Tm1l = np.array([   [1, 0,  0,  0],
-                    [0, 1,  0,  0],
-                    [0, 0,  1,  0],
-                    [0, 0,  0,  1]  ])
+    if motor1_CW:   k = 1
+    else:           k = -1
 
-#Angular rotation 
-Tm1a = np.array([   [1, 0               , 0                 , 0],       #X axis (no changes because of angle)
-                    [0, math.cos(alpha) , -math.sin(alpha)  , 0],       #Rotation in YZ plane
-                    [0, math.sin(alpha) ,  math.cos(alpha)  , 0],
-                    [0, 0               , 0                 , 1]    ])
+    alpha = math.radians(alpha)
 
+    #Tooltip shift matrix
+    motor1_x_shift = 117.5 #53.0 + 64.5  
+    motor1_y_shift = -39.5
+    motor1_z_shift = 0
 
-T = np.matmul(Tm1a, Tm1l)   #Linear shift mounted on motor
-T = np.matmul(T, Tm1s)      #Linear shifts because of design
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-fi = math.radians(10)
-print(fi)
-
-motor2_x_shift = 0
-motor2_y_shift = 53.0
-motor2_z_shift = 155.2
-
-#Mechanical shift because of design
-Tm2s = np.array([   [1, 0,  0,  motor2_x_shift],
-                    [0, 1,  0,  motor2_y_shift],
-                    [0, 0,  1,  motor2_z_shift],
-                    [0, 0,  0,  1]  ])
+    #Mechanical shift because of design
+    Tm1s = np.array([   [1, 0,  0,  motor1_x_shift],
+                        [0, 1,  0,  motor1_y_shift],
+                        [0, 0,  1,  motor1_z_shift],
+                        [0, 0,  0,  1]  ])
 
 
-#Linear move mounted on rotary AXIS
-Tm2l = np.array([   [1, 0,  0,  0],
-                    [0, 1,  0,  0],
-                    [0, 0,  1,  0],
-                    [0, 0,  0,  1]  ])
+    #Linear move mounted on rotary AXIS
+    Tm1l = np.array([   [1, 0,  0,  0],
+                        [0, 1,  0,  0],
+                        [0, 0,  1,  0],
+                        [0, 0,  0,  1]  ])
 
-#Angular rotation 
-Tm2a = np.array([   [math.cos(fi) , 0 , math.sin(fi) , 0],          #Rotation affects X axis
-                    [0            , 1 , 0            , 0],
-                    [-math.sin(fi), 0 , math.cos(fi) , 0],          #Rotation affects Z axis (ie XZ plane)
-                    [0            , 0 , 0            , 1]    ])
-
-T = np.matmul(T, Tm2a)   #Linear shift mounted on motor
-T = np.matmul(T, Tm2l)   #Linear shift mounted on motor
-T = np.matmul(T, Tm2s)   #Linear shifts because of design
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-theta = math.radians(-90)
-print(theta)
-
-motor3_x_shift =  0
-motor3_y_shift = -33
-motor3_z_shift =  98
-
-#Mechanical shift because of design
-Tm3s = np.array([   [1, 0,  0,  motor3_x_shift],
-                    [0, 1,  0,  motor3_y_shift],
-                    [0, 0,  1,  motor3_z_shift],
-                    [0, 0,  0,  1]  ])
+    #Angular rotation 
+    Tm1a = np.array([   [1, 0                 , 0                   , 0],       #X axis (no changes because of angle)
+                        [0, math.cos(k*alpha) , -math.sin(k*alpha)  , 0],       #Rotation in YZ plane
+                        [0, math.sin(k*alpha) ,  math.cos(k*alpha)  , 0],
+                        [0, 0                 , 0                   , 1]    ])
 
 
-#Linear move mounted on rotary AXIS
-Tm3l = np.array([   [1, 0,  0,  0],
-                    [0, 1,  0,  0],
-                    [0, 0,  1,  0],
-                    [0, 0,  0,  1]  ])
+    T = np.matmul(Tm1a, Tm1l)   #Linear shift mounted on motor
+    T = np.matmul(T, Tm1s)      #Linear shifts because of design
 
-#Angular rotation
-Tm3a = np.array([   [math.cos(theta) , 0 , math.sin(theta) , 0],            #rotation affects X axis
-                    [0               , 1 , 0               , 0],
-                    [-math.sin(theta), 0 , math.cos(theta) , 0],            #affects Z axis
-                    [0               , 0 , 0               , 1]    ])
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    motor2_CW = True
 
-T = np.matmul(T, Tm3a)   #Linear shift mounted on motor
-T = np.matmul(T, Tm3l)   #Linear shift mounted on motor
-T = np.matmul(T, Tm3s)   #Linear shifts because of design
+    if motor2_CW:   k = 1
+    else:           k = -1
+
+    fi = math.radians(fi)
+
+    motor2_x_shift = 0
+    motor2_y_shift = 53.0
+    motor2_z_shift = 155.2
+
+    #Mechanical shift because of design
+    Tm2s = np.array([   [1, 0,  0,  motor2_x_shift],
+                        [0, 1,  0,  motor2_y_shift],
+                        [0, 0,  1,  motor2_z_shift],
+                        [0, 0,  0,  1]  ])
 
 
-R = np.dot(T, pnt)
-print(T)
-print("--------------------------------")
-print(R)
+    #Linear move mounted on rotary AXIS
+    Tm2l = np.array([   [1, 0,  0,  0],
+                        [0, 1,  0,  0],
+                        [0, 0,  1,  0],
+                        [0, 0,  0,  1]  ])
+
+    #Angular rotation 
+    Tm2a = np.array([   [math.cos(k*fi) , 0 , -math.sin(k*fi), 0],          #Rotation affects X axis
+                        [0              , 1 , 0              , 0],
+                        [-math.sin(k*fi), 0 , math.cos(k*fi) , 0],          #Rotation affects Z axis (ie XZ plane)
+                        [0              , 0 , 0              , 1]    ])
+
+    T = np.matmul(T, Tm2a)   #Linear shift mounted on motor
+    T = np.matmul(T, Tm2l)   #Linear shift mounted on motor
+    T = np.matmul(T, Tm2s)   #Linear shifts because of design
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    motor3_CW = False
+
+    if motor3_CW:   k = 1
+    else:           k = -1
+
+    theta = math.radians(theta)
+
+    motor3_x_shift =  0
+    motor3_y_shift = -33
+    motor3_z_shift =  98
+
+    #Mechanical shift because of design
+    Tm3s = np.array([   [1, 0,  0,  motor3_x_shift],
+                        [0, 1,  0,  motor3_y_shift],
+                        [0, 0,  1,  motor3_z_shift],
+                        [0, 0,  0,  1]  ])
+
+
+    #Linear move mounted on rotary AXIS
+    Tm3l = np.array([   [1, 0,  0,  0],
+                        [0, 1,  0,  0],
+                        [0, 0,  1,  0],
+                        [0, 0,  0,  1]  ])
+
+    #Angular rotation
+    Tm3a = np.array([   [math.cos(k*theta) , 0 , -math.sin(k*theta), 0],            #rotation affects X axis
+                        [0                 , 1 , 0                 , 0],
+                        [math.sin(k*theta) , 0 , math.cos(k*theta) , 0],            #affects Z axis
+                        [0                 , 0 , 0                 , 1]    ])
+
+    T = np.matmul(T, Tm3a)   #Linear shift mounted on motor
+    T = np.matmul(T, Tm3l)   #Linear shift mounted on motor
+    T = np.matmul(T, Tm3s)   #Linear shifts because of design
+
+    R = np.dot(T, pnt)
+    return (round(R[0][0], 2), round(R[1][0], 2), round(R[2][0], 2))
+
+
+if __name__ == '__main__':
+    os.system('clear')
+
+    print(calc(0, 0, 0))
+    print("\n")
+    print(calc(1, 0, 0))
+    print("\n")
+    print(calc(0, 1, 0))
+    print("\n")
+    print(calc(0, 0, -90))
